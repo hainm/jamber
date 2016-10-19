672730c76278e2d9d7adbf9558f4dd67a89aa42f
Hai Nguyen <hainm.comp@gmail.com>
2016-10-19 16:45:32 -0400

    update sander

--- jamber/sander.py
+++ jamber/sander.py
@@ -1,4 +1,8 @@
+from __future__ import absolute_import
 from subprocess import Popen
+from .base import amberbin
 
 def run(command):
+    if 'sander' not in command:
+        command = ' '.join((amberbin('sander'), command))
     return Popen(command.split())
