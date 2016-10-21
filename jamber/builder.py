e0b00c46dd2458e8577cb8396645e38adbaf8c5e
Hai Nguyen <hainm.comp@gmail.com>
2016-10-21 16:30:20 -0400

    build

---
+++ jamber/builder.py
@@ -0,0 +1,39 @@
+import pytraj as pt
+from . import leap
+from .utils import tempfolder
+
+def build_protein(seq, command):
+    '''
+
+    Parameters
+    ----------
+    seq : protein sequence
+    command : str or list of str
+        pytraj command for building secondary structure for a given residue range
+
+    Examples
+    --------
+    >>> from jamber import build
+    >>> # Ala10
+    >>> seq = "ALA ALA ALA ALA ALA ALA ALA ALA ALA ALA"
+    >>> traj = builder.build_protein(seq, ['alpha:1-10'])
+    >>> # visualize in Jupyter notebook
+    >>> traj.visualize()
+    >>> # save to pdb file
+    >>> traj.save("new.pdb")
+
+    Returns
+    -------
+    pytraj.Trajectory
+    '''
+    pdb_fn = 'seq.pdb'
+    leap_command = """
+    source leaprc.protein.ff14SB
+    x = sequence{%s}
+    savepdb x %s
+    """ % (seq, pdb_fn)
+
+    with tempfolder():
+        leap.build(leap_command)
+        traj = pt.load(pdb_fn)
+        return pt.make_structure(traj, command)
