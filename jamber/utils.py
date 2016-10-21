bd2831eca60757f3da447c751ffd972d9002fce3
Hai Nguyen <hainm.comp@gmail.com>
2016-10-21 16:40:15 -0400

    update utils

--- jamber/utils.py
+++ jamber/utils.py
@@ -13,3 +13,7 @@ def tempfolder():
   yield
   os.chdir(cwd)
   rmtree(my_temp)
+
+def get_fn(fn):
+    this_path = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(this_path, 'tests', 'data', fn)
