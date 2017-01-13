a533932ca8985f8d0fe3884a5867b2567234179e
Hai Nguyen <hainm.comp@gmail.com>
2017-01-13 17:58:15 -0500

    pytest-cov

--- devtools/travis-ci/setup_env.sh
+++ devtools/travis-ci/setup_env.sh
@@ -14,7 +14,7 @@ conda install --yes conda-build jinja2 anaconda-client pip
 
 # create myenv
 conda create -y -n myenv python=$PYTHON_VERSION
-conda install -y -n myenv nose pytest coverage pytest-coverage
+conda install -y -n myenv nose pytest coverage pytest-cov
 conda install -y -n myenv netcdf4 numpy nomkl
 
 source activate myenv
